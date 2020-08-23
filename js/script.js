caf36ecedc305f816d3ccf23cf113ab7b8df2c93
unknown <caupiais@HEXA.LOCAL>
2020-08-23 18:23:47 +0200

    hotfix

--- js/script.js
+++ js/script.js
@@ -47,9 +47,6 @@ let user,
         }
     ],
     restaurantCollection = [],
-    starSvg = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-star-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-</svg>`,
     restaurants = []
 const $ = document
 
@@ -70,9 +67,9 @@ function centerMap(location) {
         latlng = latlng.replace(')', '')
         latlng = latlng.split(', ')
         console.log(latlng)
-        console.log($.querySelector('#editor').checked)
         if ($.querySelector('#editor').checked) {
             let name = prompt("Quel est le nom du restaurant")
+            console.log(name)
             fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latlng[0]},${latlng[1]}&key=AIzaSyCPL0ytHoiwfncMjqnKtANnbi0ukEDxBFI`)
                 .then((res) => {
                     return res.json()
@@ -83,7 +80,7 @@ function centerMap(location) {
                     id = restaurantCollection.length
                     obj.address = data.results[0].formatted_address
                     obj.lat = parseFloat(latlng[0])
-                    obj.long = parseFloat(latlng[1])
+                    obj.lng = parseFloat(latlng[1])
                     obj.ratings = new Array
                     restaurantCollection.push(new Restaurant(obj, id))
                     let near = nearSort(restaurantCollection, 1)
